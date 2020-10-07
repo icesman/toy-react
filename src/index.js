2f5ebc9fcb04e6927390e8e92b87e5782972cfe1
iceman <championzheng1992@gmail.com>
2020-10-07 23:43:43 +0800

    feat: add event handler

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import {
 } from './toy-react';
 
 class HelloWorld extends Component {
+	constructor() {
+		super();
+		this.state = {
+			a: 1,
+			b: 2
+		};
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Hello world!</h1>
+				<button onClick={() => this.setState({
+					a: this.state.a + 1
+				})}>plus</button>
+				<div>{this.state.a.toString()}</div>
 				{this.children}
 			</div>
 		);
--- src/toy-react.js
+++ src/toy-react.js
@@ -3,23 +3,44 @@
  * @date: 2020/10/5.
  */
 
+let RENDER_TO_DOM = Symbol('render_to_dom');
+
 class ElementWrapper {
 	constructor(type) {
 		this.root = document.createElement(type);
 	}
 	
 	setAttribute(name, value) {
-		this.root.setAttribute(name, value);
+		if (name.match(/^on([\s\S]+)$/)) {
+			this.root.addEventListener(RegExp.$1.replace(/^([\s\S])/, text => text.toLowerCase()), value);
+		} else if (name === 'className') {
+			this.root.setAttribute('class', value);
+		} else {
+			this.root.setAttribute(name, value);
+		}
 	}
 	
 	appendChild(component) {
-		component && this.root.appendChild(component.root);
+		let range = document.createRange();
+		range.setStart(this.root, this.root.childNodes.length);
+		range.setEnd(this.root, this.root.childNodes.length);
+		component[RENDER_TO_DOM](range);
+	}
+	
+	[RENDER_TO_DOM](range) {
+		range.deleteContents();
+		range.insertNode(this.root);
 	}
 }
 
 class TextWrapper {
 	constructor(content) {
 		this.root = document.createTextNode(content);
+	}
+	
+	[RENDER_TO_DOM](range) {
+		range.deleteContents();
+		range.insertNode(this.root);
 	}
 }
 
@@ -28,6 +49,9 @@ export class Component {
 		this.props = Object.create(null);
 		this.children = [];
 		this._root = null;
+		this._range = null;
+		// todo: confirm if ok
+		// this.state = null;
 	}
 	
 	setAttribute(name, value) {
@@ -38,12 +62,47 @@ export class Component {
 		component && this.children.push(component);
 	}
 	
-	get root() {
-		if (!this._root) {
-			// 会发生递归
-			this._root = this.render().root;
+	[RENDER_TO_DOM](range) {
+		this._range = range;
+		this.render()[RENDER_TO_DOM](range);
+	}
+	
+	render() {}
+	
+	reRender() {
+		let oldRange = this._range;
+		
+		let range = document.createRange();
+		range.setStart(oldRange.startContainer, oldRange.startOffset);
+		range.setEnd(oldRange.startContainer, oldRange.startContainer);
+		this[RENDER_TO_DOM](range);
+		
+		oldRange.setStart(range.endContainer, range.endOffset);
+		oldRange.deleteContents();
+	}
+	
+	setState(newState) {
+		if (this.state === null && typeof this.state !== 'object') {
+			this.state = newState;
+			this.reRender();
+			return;
 		}
-		return this._root;
+		
+		let merge = (oldState, newState) => {
+			for (let property in newState) {
+				if (oldState[property] === null
+					|| typeof oldState[property] !== 'object') {
+					oldState[property] = newState[property];
+				} else {
+					merge(oldState[property], newState[property]);
+				}
+			}
+		};
+		
+		// 这里直接merge 后能改变，
+		// 是因为对象类型是引用类型
+		merge(this.state, newState);
+		this.reRender();
 	}
 }
 
@@ -54,7 +113,7 @@ export function createElement(type, attributes, ...children) {
 	} else {
 		// jsx 会将自定义组件当成对象传入
 		// 所以需要区分处理
-		ele = new type;
+		ele = new type();
 	}
 
 	for (let p in attributes) {
@@ -65,7 +124,9 @@ export function createElement(type, attributes, ...children) {
 		for (let child of children) {
 			if (typeof child === 'string') {
 				child = new TextWrapper(child);
-				ele.appendChild(child);
+			}
+			if (child === null) {
+				continue;
 			}
 			if ((typeof child === 'object') && (child instanceof Array)) {
 				insertChildren(child);
@@ -81,5 +142,10 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-	parentElement.appendChild(component.root);
+	let range = document.createRange();
+	
+	range.setStart(parentElement, 0);
+	range.setEnd(parentElement, parentElement.childNodes.length);
+	range.deleteContents();
+	component[RENDER_TO_DOM](range);
 }
